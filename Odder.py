a44de670b29a16e6468cc3719d99aec423b8e14c
Merculous <Merculous@users.noreply.github.com>
2020-02-10 21:45:12 -0800

    Update some stuff, patchfile is pretty much broken cause I suck with with using diff

--- Odder.py
+++ Odder.py
@@ -3,13 +3,36 @@
 import argparse
 import os
 import sqlite3
+import subprocess
 import sys
+import shutil
+
 
 """
 
-In order to install WhiteTails, you have to disable SIP. Enter recovery and enter csrutil disable.
+In order to install HomeDiagnostic, you have to disable SIP. Enter recovery and enter csrutil disable.
 
-Devices supported out-of-the-box:
+Devices supported out-of-the-box: (13F40)
+    940:
+        iPhone 4S
+            n94
+        iPad 2
+            k93, k94, k95
+    942:
+        iPad 2
+            k93a
+        iPod Touch 5th Gen
+            n78
+        AppleTV 3rd Gen
+            j33
+        iPad Mini
+            p105, p106, p107
+    945:
+        iPad 3rd Gen
+            j1, j2, j2a
+    947:
+        AppleTV 3rd Gen
+            j33i
     950:
         iPhone 5
             n41, n42
@@ -19,108 +42,125 @@ Devices supported out-of-the-box:
         iPad 4G
             p101, p102, p103
             
-64-bit cannot be built
+64-bit cannot be built (without reversing)
         
 brew cask install db-browser-for-sqlite (This is to view the contents of device_map.db)
 
 TODO: 
 
-    - Build SecureROM
+    - Build SecureROM (if possible)
 
-    - Add missing data to build EmbeddedIOP for other devices (only builds for a few as of now)
-    
-    - Print all of supported devices (data: Target, TargetType, Platform, ChipID, SecurityEpoch, CryptoHashMethod,
-    ProductID, ImageFormat) Check with makefiles/device_map.mk
-    
-    - Hack up some janky Python code to add custom devices
-    
-    - Figure out how to parse args like this: ./Odder.py --build --device n41
+    - Better patching process
 
+"""
+# -----------------
+"""
+Patching process:
+    - ./Makefile [line 37] // iphoneos.internal > iphoneos [./Makefile line 37]
+    - ./tools/generate_debug_hashes.py // chmod +x 
+    - ./tools/check_liblist.py // chmod +x
+    - ./makefiles/config.mk [line 27, 30, 33] // $SDKVERSION -> 10.0
+    - ./drivers/flash_nand/ppn-swiss/ppn.c [line 43] // count %zd\n -> count %u\n
+    - ./lib/fs/fs.c [line 395] // memset((void *)addr, *maxsize, 0); -> memset((void *)addr, *maxsize, (0));
+    - ./makefiles/build.mk [line 25] // Remove -lcompiler_rt-static
+    - ./tools/macho_post_process.py // chmod +x
+Optional:
+    - ./tools/tools.mk [line 49] // /usr/local/bin/img4 (if you somehow are 1337 h4^X0r, this is useful)
 """
 
 
-def applyPatch():
-    os.system('git apply patchfile')
+def applyPatch(): # TODO Either add some code to change, or make a better patch file or similar
+    #subprocess.run(['git', 'apply', '--verbose', 'patchfile'], stdout=subprocess.PIPE)
+    # If you can somehow use the provided patchfile (not from git diff this time)
+    pass
 
 
-def build():
-    application = ['iBoot', 'SecureROM', 'EmbeddedIOP']  # SecureROM is not working
-    devices_all = 'n41 n42 n48 n49 p101 p102 p103'  # Working out of the box. 950 and 955 devices.
+def build(application=None, device=None):
 
-    print(f'Building iBoot for device(s): {devices_all}!')
-    os.system(f'make APPLICATIONS="iBoot" TARGETS="{devices_all}"')
+    clean()
+    checkFiles() # Always make sure that everything is good before we start
+
+    if application == 'EmbeddedIOP':
+        print('Making EmbeddedIOP...')
+        subprocess.run(['make', 'APPLICATIONS=EmbeddedIOP'], stdout=subprocess.PIPE) # Device was passed
+
+    elif application == 'iBoot':
+        if device is not None:
+            print('Making iBoot with device(s):', device)
+            subprocess.run(['make', 'APPLICATIONS=iBoot', f'TARGETS={device}'], stdout=subprocess.PIPE) # Device was passed
+        else:
+            sys.exit('No devices were passed!')
+
+    elif application == 'SecureROM':
+        print('Making SecureROM...')
+        subprocess.run(['make', 'VERBOSE=YES', 'APPLICATIONS=SecureROM', 'IMAGE_FORMAT=img3'], stdout=subprocess.PIPE)
+
+    else:
+        sys.exit('Invaild application, got:', application)
 
 
 def clean():
     if os.path.exists('build'):
-        print('Cleaning up!')
-        os.system('make clean')
-    else:
-        print('build folder not found, not cleaning!')
+        shutil.rmtree('build')
 
 
 def checkFiles():
     device_map = '/usr/local/standalone/firmware/device_map.db'  # Provided from HomeDiagnostic
     xcode_path = '/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/usr/'  # Path for device_map.db
-    dirtomake = f'{xcode_path}local/standalone/firmware/'  # We need to make this so we can build
+    dirtomake = f'{xcode_path}local/standalone/firmware/'  # We need to make this so we can build (currently symlinked to local device_map.db)
 
     # Check if device_map.db exists
 
-    if os.path.exists(device_map):
-        print(f'{device_map} exists!')
-    else:
+    if not os.path.exists(device_map):
         sys.exit(f'Did not find device_map.db at: {device_map}')
 
     # Check if we need to make the paths in Xcode
 
-    if os.path.exists(dirtomake):
-        print(f'{dirtomake} exists!')
-    else:
+    if not os.path.exists(dirtomake):
         print(f'{dirtomake} does not exist, we have to grant sudo privileges here...')
-        os.system(f'sudo mkdir -p {dirtomake}')
-
+        subprocess.run(["sudo", "mkdir", "-p", dirtomake], stdout=subprocess.PIPE)
+    
     # Check if device_map.db exits in Xcode path
 
-    if os.path.exists(f'{dirtomake}device_map.db'):
-        print(f'{dirtomake}device_map.db exists!')
-    else:
+    if not os.path.exists(f'{dirtomake}device_map.db'):
         print('Copying device_map.db to its necessary location in Xcode. We have to grant sudo privileges here...')
-        os.system(f'sudo cp -rv {device_map} {dirtomake}device_map.db')
+        subprocess.run(["sudo", "cp", "-rv", device_map, f"{dirtomake}device_map.db"], stdout=subprocess.PIPE)
 
 
-def test():
-    conn = sqlite3.connect('device_map.db')
+def convertListToSingleString(data):
+    devices = ''
+
+    # TODO I'm sure there's a more simple and obviously better choice of code, fix?
+    for target in data:
+        shit = ''.join(target)
+        devices += " " + shit
+    return devices.strip()
+
+
+def devices(): 
+    conn = sqlite3.connect('/usr/local/standalone/firmware/device_map.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT Target FROM Targets')
-    devices = cursor.fetchall()
-    print(devices)
+    thingy = cursor.execute("SELECT DISTINCT TargetType ImageFormat FROM Targets WHERE ImageFormat in ('img3')") # Returns all 32-bit devices :D
+    return convertListToSingleString(thingy)
 
 
-if __name__ == '__main__':
-    argv = sys.argv
-    argc = len(argv)
-
-    parser = argparse.ArgumentParser()
-    # parser.add_argument("--add", help="Add device to device_map database", action="store_true")
+def main():
+    parser = argparse.ArgumentParser(usage=f"{sys.argv[0]} <option>")
     parser.add_argument("--apply-patch", help="Apply patches from patchfile to enable compiling", action="store_true")
     parser.add_argument("--build", help="Start the build process", action="store_true")
     parser.add_argument("--clean", help="Clean up", action="store_true")
     parser.add_argument("--check", help="This will ensure you have all of the required files", action="store_true")
-    # parser.add_argument("--device", help="Compile with the given device(s)", action="store_true")
     parser.add_argument("--devices", help="Get all of the supported devices", action="store_true")
-    # parser.add_argument("--EmbeddedIOP", help="Compile EmbeddedIOP only", action="store_true")
-    # parser.add_argument("--iBoot", help="Compile iBoot only", action="store_true")
-    # parser.add_argument("--SecureROM", help="Compile SecureROM only", action="store_true")
 
     args = parser.parse_args()
 
     if args.apply_patch:
-        applyPatch()
+        #applyPatch()
+        print('View the code of this script, it has all of the info provided to apply the patches')
 
     elif args.build:
-        clean()
-        checkFiles()
-        build()
+        #targets = devices() # Default, compile iBoot with all supported 32-bit devices
+        build('iBoot', 'n94')
 
     elif args.clean:
         clean()
@@ -129,7 +169,25 @@ if __name__ == '__main__':
         checkFiles()
 
     elif args.devices:
-        test()
+        print(devices())
 
     else:
-        sys.exit(parser.print_help())
+        sys.exit(parser.print_help(sys.stderr))
+
+
+if __name__ == '__main__':
+   main() 
+
+"""
+Road to compiling the only provided 32-bit ROM, with weird (to me) directories:
+    drivers/apple/aes_v2/aes_v2.h:16:10: fatal error: 'soc/t8002/b0/module/aes.h' file not found
+    ./drivers/samsung/aes/aes.h
+    ./include/drivers/aes.h
+    ./platform/t8002/include/platform/soc/spds/aes.h
+    socgen
+    ./platform/t8002/b0
+    ./platform/s8000/include/platform/soc/s8000/b0
+    ./platform/s8000/include/platform/soc/s8001/b0
+    platform/s8000/include/platform/soc/s8000/b0
+    make VERBOSE=YES APPLICATIONS=SecureROM TARGETS=t8002 IMAGE_FORMAT=img3
+"""
