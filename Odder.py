739ca02926ad871eee3316bab91882eb8387f946
Merculous <24739823+Merculous@users.noreply.github.com>
2020-05-12 19:41:10 -0700

    Format, remove dumb function

--- Odder.py
+++ Odder.py
@@ -53,6 +53,7 @@ TODO:
 
 """
 
+
 def doPatches(filepath, stockString, patchString, stringLine):
     # Bad patcher by Matty (@mosk_i)
     print("Patching", filepath, " at line", stringLine)
@@ -66,28 +67,37 @@ def doPatches(filepath, stockString, patchString, stringLine):
                 data[stringLine] = str(patchString)
                 f.close()
                 g = open(filepath, "wt")
-                g.writelines( data )
+                g.writelines(data)
                 g.close()
                 # changing file permissions
-                os.chmod(filepath, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH)
+                os.chmod(filepath, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR |
+                         stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH)
                 return
             else:
-                print("Didn't find '", stockString, "' in", filepath, " at line ", stringLine, " Moving on to next patch...") 
+                print("Didn't find '", stockString, "' in", filepath,
+                      " at line ", stringLine, " Moving on to next patch...")
                 return
     else:
-        print("Couldn't find file at '", filepath,"'. Moving on to next patch...")
+        print("Couldn't find file at '", filepath,
+              "'. Moving on to next patch...")
         return
 
-            
 
-def applyPatch(): # TODO Make it not replace the entire line, only what is specified
-    doPatches("Makefile", "export SDK_PLATFORM	?=	iphoneos.internal", "export SDK_PLATFORM	?=	iphoneos\n", 36)
-    doPatches("makefiles/config.mk", "DEPLOYMENT_TARGET_FLAGS	=	-mwatchos-version-min=$(SDKVERSION)", "DEPLOYMENT_TARGET_FLAGS	=	-mwatchos-version-min=10.0\n", 26)
-    doPatches("makefiles/config.mk", "DEPLOYMENT_TARGET_FLAGS	=	-mtvos-version-min=$(SDKVERSION)", "DEPLOYMENT_TARGET_FLAGS	=	-mtvos-version-min==10.0\n", 29)
-    doPatches("makefiles/config.mk", "DEPLOYMENT_TARGET_FLAGS	=	-miphoneos-version-min=$(SDKVERSION)", "DEPLOYMENT_TARGET_FLAGS	=	-miphoneos-version-min=10.0\n", 32)
-    doPatches("drivers/flash_nand/ppn-swiss/ppn.c", r'			printf("nand_read_block_hook: failure %d reading block %u, count %zd\n", err, block, count);', r'			printf("nand_read_block_hook: failure %d reading block %u, count %u\n", err, block, count);' + "\n", 42)
-    doPatches("lib/fs/fs.c", r"		memset((void *)addr, *maxsize, 0);", r"		memset((void *)addr, *maxsize, (0));" + "\n", 394)
-    doPatches("makefiles/build.mk", "_RUNTIME_FLAGS		:=	-L$(SDKROOT)/usr/local/lib -lcompiler_rt-static $(LIBBUILTIN_BUILD)", "_RUNTIME_FLAGS		:=	-L$(SDKROOT)/usr/local/lib $(LIBBUILTIN_BUILD)\n", 24)
+def applyPatch():  # TODO Make it not replace the entire line, only what is specified
+    doPatches("Makefile", "export SDK_PLATFORM	?=	iphoneos.internal",
+              "export SDK_PLATFORM	?=	iphoneos\n", 36)
+    doPatches("makefiles/config.mk", "DEPLOYMENT_TARGET_FLAGS	=	-mwatchos-version-min=$(SDKVERSION)",
+              "DEPLOYMENT_TARGET_FLAGS	=	-mwatchos-version-min=10.0\n", 26)
+    doPatches("makefiles/config.mk", "DEPLOYMENT_TARGET_FLAGS	=	-mtvos-version-min=$(SDKVERSION)",
+              "DEPLOYMENT_TARGET_FLAGS	=	-mtvos-version-min==10.0\n", 29)
+    doPatches("makefiles/config.mk", "DEPLOYMENT_TARGET_FLAGS	=	-miphoneos-version-min=$(SDKVERSION)",
+              "DEPLOYMENT_TARGET_FLAGS	=	-miphoneos-version-min=10.0\n", 32)
+    doPatches("drivers/flash_nand/ppn-swiss/ppn.c", r'			printf("nand_read_block_hook: failure %d reading block %u, count %zd\n", err, block, count);',
+              r'			printf("nand_read_block_hook: failure %d reading block %u, count %u\n", err, block, count);' + "\n", 42)
+    doPatches("lib/fs/fs.c", r"		memset((void *)addr, *maxsize, 0);",
+              r"		memset((void *)addr, *maxsize, (0));" + "\n", 394)
+    doPatches("makefiles/build.mk", "_RUNTIME_FLAGS		:=	-L$(SDKROOT)/usr/local/lib -lcompiler_rt-static $(LIBBUILTIN_BUILD)",
+              "_RUNTIME_FLAGS		:=	-L$(SDKROOT)/usr/local/lib $(LIBBUILTIN_BUILD)\n", 24)
     # Following patch is optional. Feel free to uncomment it if you want to (Make sure you have img4 installed to /usr/local/bin/img4)
     #doPatches("tools/tools.mk", "export IMG4PAYLOAD	:=	$(shell xcrun -sdk $(SDKROOT) -find img4payload)", "export IMG4PAYLOAD	:=	/usr/local/bin/img4", 48)
     print("\nPatches are done!")
@@ -97,22 +107,26 @@ def applyPatch(): # TODO Make it not replace the entire line, only what is speci
 def build(application=None, device=None):
 
     clean()
-    checkFiles() # Always make sure that everything is good before we start
+    checkFiles()  # Always make sure that everything is good before we start
 
     if application == 'EmbeddedIOP':
         print('Making EmbeddedIOP...')
-        subprocess.run(['make', 'APPLICATIONS=EmbeddedIOP'], stdout=subprocess.PIPE) # Device was passed
+        subprocess.run(['make', 'APPLICATIONS=EmbeddedIOP'],
+                       stdout=subprocess.PIPE)  # Device was passed
 
     elif application == 'iBoot':
         if device is not None:
             print('Making iBoot with device(s):', device)
-            subprocess.run(['make', 'APPLICATIONS=iBoot', f'TARGETS={device}'], stdout=subprocess.PIPE) # Device was passed
+            # Device was passed
+            subprocess.run(['make', 'APPLICATIONS=iBoot',
+                            f'TARGETS={device}'], stdout=subprocess.PIPE)
         else:
             sys.exit('No devices were passed!')
 
     elif application == 'SecureROM':
         print('Making SecureROM...')
-        subprocess.run(['make', 'VERBOSE=YES', 'APPLICATIONS=SecureROM', 'IMAGE_FORMAT=img3'], stdout=subprocess.PIPE)
+        subprocess.run(['make', 'VERBOSE=YES', 'APPLICATIONS=SecureROM',
+                        'IMAGE_FORMAT=img3'], stdout=subprocess.PIPE)
 
     else:
         sys.exit('Invaild application, got:', application)
@@ -124,9 +138,12 @@ def clean():
 
 
 def checkFiles():
-    device_map = '/usr/local/standalone/firmware/device_map.db'  # Provided from HomeDiagnostic
-    xcode_path = '/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/usr/'  # Path for device_map.db
-    dirtomake = f'{xcode_path}local/standalone/firmware/'  # We need to make this so we can build (currently symlinked to local device_map.db)
+    # Provided from HomeDiagnostic
+    device_map = '/usr/local/standalone/firmware/device_map.db'
+    # Path for device_map.db
+    xcode_path = '/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/usr/'
+    # We need to make this so we can build (currently symlinked to local device_map.db)
+    dirtomake = f'{xcode_path}local/standalone/firmware/'
 
     # Check if device_map.db exists
 
@@ -137,39 +154,37 @@ def checkFiles():
 
     if not os.path.exists(dirtomake):
         print(f'{dirtomake} does not exist, we have to grant sudo privileges here...')
-        subprocess.run(["sudo", "mkdir", "-p", dirtomake], stdout=subprocess.PIPE)
-    
+        subprocess.run(["sudo", "mkdir", "-p", dirtomake],
+                       stdout=subprocess.PIPE)
+
     # Check if device_map.db exits in Xcode path
 
     if not os.path.exists(f'{dirtomake}device_map.db'):
         print('Copying device_map.db to its necessary location in Xcode. We have to grant sudo privileges here...')
-        subprocess.run(["sudo", "cp", "-rv", device_map, f"{dirtomake}device_map.db"], stdout=subprocess.PIPE)
+        subprocess.run(["sudo", "cp", "-rv", device_map,
+                        f"{dirtomake}device_map.db"], stdout=subprocess.PIPE)
 
 
-def convertListToSingleString(data):
-    devices = ''
-
-    # TODO I'm sure there's a more simple and obviously better choice of code, fix?
-    for target in data:
-        shit = ''.join(target)
-        devices += " " + shit
-    return devices.strip()
-
-
-def devices(): 
+def devices():
     conn = sqlite3.connect('/usr/local/standalone/firmware/device_map.db')
     cursor = conn.cursor()
-    thingy = cursor.execute("SELECT DISTINCT TargetType ImageFormat FROM Targets WHERE ImageFormat in ('img3')") # Returns all 32-bit devices :D
-    return convertListToSingleString(thingy)
+    # Returns all 32-bit devices :D
+    thingy = cursor.execute(
+        "SELECT DISTINCT TargetType ImageFormat FROM Targets WHERE ImageFormat in ('img3')")
+    return ''.join([thingy])
 
 
 def main():
     parser = argparse.ArgumentParser(usage=f"{sys.argv[0]} <option>")
-    parser.add_argument("--apply-patch", help="Apply patches from patchfile to enable compiling", action="store_true")
-    parser.add_argument("--build", help="Start the build process", action="store_true")
+    parser.add_argument(
+        "--apply-patch", help="Apply patches from patchfile to enable compiling", action="store_true")
+    parser.add_argument(
+        "--build", help="Start the build process", action="store_true")
     parser.add_argument("--clean", help="Clean up", action="store_true")
-    parser.add_argument("--check", help="This will ensure you have all of the required files", action="store_true")
-    parser.add_argument("--devices", help="Get all of the supported devices", action="store_true")
+    parser.add_argument(
+        "--check", help="This will ensure you have all of the required files", action="store_true")
+    parser.add_argument(
+        "--devices", help="Get all of the supported devices", action="store_true")
 
     args = parser.parse_args()
 
@@ -177,7 +192,7 @@ def main():
         applyPatch()
 
     elif args.build:
-        targets = devices() # Default, compile iBoot with all supported 32-bit devices
+        targets = devices()  # Default, compile iBoot with all supported 32-bit devices
         build('iBoot', targets)
 
     elif args.clean:
@@ -194,7 +209,7 @@ def main():
 
 
 if __name__ == '__main__':
-   main() 
+    main()
 
 """
 Road to compiling the only provided 32-bit ROM, with weird (to me) directories:
